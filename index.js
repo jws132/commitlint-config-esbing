bcae16a08d1222aab14eae7c0a164b24bbcdb6ad
jws132 <jiangwenshu1990@126.com>
2022-02-02 13:22:52 +0800

    fix: asd
    asd

--- index.js
+++ index.js
@@ -1,51 +1,52 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional','cz'],
+  extends: ['@commitlint/config-conventional', 'cz'],
+
   rules: {
     // <type> 不能为空
     'scope-empty': [2, 'always'],
     // <type> 不能为空
-    'type-empty': [2, 'never'], 
+    'type-empty': [2, 'never'],
     // <type>格式小写
-    'type-case': [2, 'always', 'lower-case'], 
+    'type-case': [2, 'always', 'lower-case'],
     // <scope> 格式 小写
-    'scope-case': [2, 'always', 'lower-case'], 
+    'scope-case': [2, 'always', 'lower-case'],
     // <subject> 不能为空
-    'subject-empty': [2, 'never'], 
+    'subject-empty': [2, 'never'],
     // <subject> 以.为结束标志
-    'subject-full-stop': [2, 'never', '.'], 
+    'subject-full-stop': [2, 'never', '.'],
     'subject-case': [2, 'never', []],
     // body换行
-    'body-leading-blank': [2, 'always'], 
+    'body-leading-blank': [2, 'always'],
     // <footer> 以空行开头
-    'footer-leading-blank': [1, 'always'], 
+    'footer-leading-blank': [1, 'always'],
     // header 最长72
-    'header-max-length': [2, 'never', 72], 
+    'header-max-length': [2, 'never', 72],
     'type-enum': [
-      2, 
+      2,
       'always',
       [
         // 编译相关的修改，例如发布版本、对项目构建或者依赖的改动
-        'build', 
+        'build',
         // 其他修改, 比如改变构建流程、或者增加依赖库、工具等
-        'chore', 
+        'chore',
         // 文档修改
-        'docs', 
+        'docs',
         // 新特性、新功能
-        'feat', 
+        'feat',
         // 修改bug
-        'fix', 
+        'fix',
         // 优化相关，比如提升性能、体验
-        'perf', 
+        'perf',
         // 代码重构
-        'refactor', 
+        'refactor',
         // 回滚到上一个版本
-        'revert', 
+        'revert',
         // 代码格式修改, 注意不是 css 修改
         'style',
-		// 添加依赖
-		'add',
+        // 添加依赖
+        'add',
         // 测试用例修改
-        'test', 
+        'test',
         // Add custom type
         'ui', // Changes page layout and style
         'release', // 发布
